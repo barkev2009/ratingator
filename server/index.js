222bb811a290016f6f09a0503344480b2a0cab76
Greg Kovshov <barkev2009@bk.ru>
2023-10-31 13:36:36 +0700

    initialize project

---
+++ server/index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const fileUpload = require('express-fileupload');
+
+const router = require('./routers/index');
+const errorHandler = require('./middleware/ErrorHandlerMiddleware');
+const sequelize = require('./db');
+
+const PORT = process.env.PORT || 5003;
+
+const app = express()
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
+app.use('/api', router);
+
+// Middleware с ошибками должен регистрироваться в последнюю очередь!!!
+app.use(errorHandler);
+
+app.get(
+    '/',
+    (req, resp) => {
+        resp.status(200).json(
+            {
+                message: 'Ratingator ready!!!'
+            }
+        );
+    }
+)
+
+const server = http.createServer(app);
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        server.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+start();
