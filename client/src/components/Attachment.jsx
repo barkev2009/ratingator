5be36968103934d3bad7a0bd1e71f84881173076
Greg Kovshov <barkev2009@bk.ru>
2023-11-06 22:48:34 +0700

    delete attach

--- client/src/components/Attachment.jsx
+++ client/src/components/Attachment.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 import Modal from '../common/Modal';
 import styles from '../css/Attachment.module.css';
+import Trash from '../svg/Trash';
+import { useDispatch } from 'react-redux';
+import { deleteAttachment } from '../reducers/attachments';
 
 const Attachment = ({ attachment }) => {
 
     const [active, setActive] = useState(false);
+    const dispatch = useDispatch();
+
+    const deleteHandler = () => {
+        dispatch(deleteAttachment({ id: attachment.id }));
+        setActive(false);
+    }
 
     return (
         <div>
             <img onClick={() => setActive(true)} className={styles.thumbnail} src={attachment.path} alt={attachment.id} />
             <Modal active={active} setActive={setActive}>
-                <div className={styles.imgContainer}>
+                <div className={styles.imgContainer} onClick={deleteHandler}>
                     <img width={'100%'} src={attachment.path} alt={attachment.id} />
+                    <div>
+                        <Trash />
+                    </div>
                 </div>
             </Modal>
         </div>
